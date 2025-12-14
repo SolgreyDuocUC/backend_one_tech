44b7c31a102c9135d3221f41c92a3d81d8c704c6
Solgrey <sol.medina@duocuc.cl>
2025-12-14 15:57:57 -0300

    Consistencia de roles

--- src/main/java/backend_one_tech/dto/user/userDTOs/UserUpdateDTO.java
+++ src/main/java/backend_one_tech/dto/user/userDTOs/UserUpdateDTO.java
@@ -3,16 +3,18 @@ package backend_one_tech.dto.user.userDTOs;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 public class UserUpdateDTO {
+
     private String nombre;
     private String apellidos;
     private String direccion;
     private String region;
     private String comuna;
     private String genero;
-    private List<String> roles;
+
+    private Set<Long> roleIds;
 }
