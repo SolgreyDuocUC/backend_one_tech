5011b3915e4dc0354e981af8fec036c2d2a5a109
Solgrey <sol.medina@duocuc.cl>
2025-12-16 14:05:56 -0300

    Cambios de gestión prod

--- src/main/java/backend_one_tech/dto/Auth/AuthResponse.java
+++ src/main/java/backend_one_tech/dto/Auth/AuthResponse.java
@@ -3,10 +3,14 @@ package backend_one_tech.dto.Auth;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class AuthResponse {
     private String accessToken;
     private String refreshToken;
+    private String email;
+    private List<String> roles;
 }
 
